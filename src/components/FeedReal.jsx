edde365e67246ded1ec568758d839d8cf6360d70
AdrianOjeda <adrian.16300022@gmail.com>
2024-04-25 01:23:44 -0600

    Resolution issue fix

--- src/components/FeedReal.jsx
+++ src/components/FeedReal.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import HeaderReal from "./HeaderReal";
+import DisplayBooks from "./DisplayBooks";
 
 function FeedReal()
 {
 
-    return <div className="divHeader">
-        <HeaderReal/>
+    return <div>
+        <div className="divHeader">
+            <HeaderReal/>
         
+        </div>
+        <div>
+            <DisplayBooks/>
+        </div>
+
     </div>
 
 }
