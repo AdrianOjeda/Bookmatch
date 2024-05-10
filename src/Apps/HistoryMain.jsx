64ed3ecfe6f2a510273254f105d9f0d023069ed1
Cesar Aguilar <c.ivanovich.214@gmail.com>
2024-05-10 00:34:50 -0600

    Front de el historial

---
+++ src/Apps/HistoryMain.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Historial from '../Pages/History'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    <Historial/>
+  
+)
