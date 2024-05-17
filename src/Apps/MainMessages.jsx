b1343b5ae44c42e20d3e7eb907fb999c9f77e110
Cesar Aguilar <c.ivanovich.214@gmail.com>
2024-05-17 02:29:19 -0600

    Front Chat

--- src/Apps/MainMessages.jsx
+++ src/Apps/MainMessages.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Messages from '../Pages/Messages'
-
+import '../styles/messages.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     <Messages/>
